bfb77886f5f750d7ba123c0fc99f457a8710705b
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-03-21 03:32:57 +0600

    Create HashMapExample.rs

---
+++ DataTypes/HashMapExample.rs
@@ -0,0 +1,8 @@
+use std::collections::HashMap;
+
+fn main() {
+    let mut MyHashMap = HashMap::new();
+    MyHashMap.insert("Key1", 10);
+    MyHashMap.insert("Key2", 20);
+    println!("MyHashMap: {:?}", MyHashMap);
+}
