dc68f9048db24475e8856f949b8dcf2c61665430
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-03-21 04:16:22 +0600

    Create MatchExample.rs

---
+++ ControlFlow/MatchExample.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let day = 3;
+    match day {
+        1 => println!("Monday"),
+        2 => println!("Tuesday"),
+        3 => println!("Wednesday"),
+        _ => println!("Other day"),
+    }
+}
