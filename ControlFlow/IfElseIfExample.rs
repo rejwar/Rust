dd05a65beb968f62b6884e15a2a6fb0f4dfe81c1
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-03-21 04:16:04 +0600

    Create IfElseIfExample.rs

---
+++ ControlFlow/IfElseIfExample.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let number = 0;
+    if number > 0 {
+        println!("Positive");
+    } else if number == 0 {
+        println!("Zero");
+    } else {
+        println!("Negative");
+    }
+}
