a0aae63d820d31e0fc34c256cbd3e5b7a98f533c
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-03-20 22:54:19 +0600

    Create MutableHashSetExample.rs

---
+++ Mutability/MutableHashSetExample.rs
@@ -0,0 +1,8 @@
+use std::collections::HashSet;
+
+fn main() {
+    let mut UniqueNumbers = HashSet::new();
+    UniqueNumbers.insert(10);
+    UniqueNumbers.insert(20);
+    println!("UniqueNumbers: {:?}", UniqueNumbers);
+}
