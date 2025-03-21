1c36c257c05a1cc169c23d73eeebc32b0d1d985b
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-03-21 06:29:43 +0600

    Create StructWithMethod.rs

---
+++ Structs/StructWithMethod.rs
@@ -0,0 +1,18 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
+fn main() {
+    let rect = Rectangle {
+        width: 10,
+        height: 20,
+    };
+    println!("Area: {}", rect.area());
+}
