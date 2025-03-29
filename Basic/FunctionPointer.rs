9308c6e365d08071c125468b7256e3f7c68edfef
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-03-29 10:16:57 +0600

    Create FunctionPointer.rs

---
+++ Basic/FunctionPointer.rs
@@ -0,0 +1,13 @@
+fn Add(a: i32 , b: i32) -> i32 {
+
+    a+b
+}
+
+
+fn Calculate(f: fn(i32 , i32) -> i32, x: i32 , y: i32) {
+    println!("Result : {}" , f(x,y));
+}
+
+fn main() {
+    Calculate(Add, 10, 20);
+}
