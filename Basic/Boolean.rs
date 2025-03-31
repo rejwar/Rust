9a1a8fa7a724ddc5c190d9a5d30f6dcd8350d576
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-01 05:27:02 +0600

    Create Boolean.rs

---
+++ Basic/Boolean.rs
@@ -0,0 +1,12 @@
+fn ShowBoolean()
+{
+    let IsRustFun: bool = true;
+    let IslearningHard: bool = false;
+
+    println!(" IS Rust fun ?{}", IsRustFun);
+    println!("IS learning Rust hard {}", IslearningHard);
+}
+
+fn main(){
+    ShowBoolean();
+}
