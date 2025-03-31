dcadd58c46faeb3004fa8322cfcd0bcbe50a45af
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-03-31 18:52:46 +0600

    Create IsMale.rs

---
+++ Basic/IsMale.rs
@@ -0,0 +1,19 @@
+fn main()
+{
+
+    let IsMale = false;
+    let IsAbove18 = true;
+
+    if IsMale{
+        println!("You are male");
+    }
+    else {
+        println!("You are female");
+
+    }
+
+    if IsMale && IsAbove18{
+        println!("You are a legal male");
+
+    }
+    }
