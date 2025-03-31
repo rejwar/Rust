8ccba0173a1fab6b911aa9aeee04ef4a4fa32715
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-01 05:04:58 +0600

    Create FloatingPoints.rs

---
+++ Basic/FloatingPoints.rs
@@ -0,0 +1,12 @@
+fn ShowloatingPoint(){
+    let float_number: f32 =3.14;
+    let double_precidon: f64  = 2.458972934857;
+
+    println!("FloatNumber :m{}" , float_number);
+    println!("Double precison Number: {}", double_precidon);
+
+}
+
+fn main(){
+    ShowloatingPoint();
+}
