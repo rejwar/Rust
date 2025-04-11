7957fc5ea170e97b97cde92aef271b5076c6dfa2
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-11 11:39:24 +0600

    Create Integers.rs

---
+++ ScalarTypes/Integers.rs
@@ -0,0 +1,14 @@
+fn main()
+{
+    let PositiveNumber: i32 = 42;
+
+    let NegativeNumber: i32 = -43;
+
+    let Zero: i32 = 0;
+
+
+    println!("Positve Number: {}", PositiveNumber);
+    println!("Negative Number: {}", NegativeNumber);
+    println!("Zero: {}", Zero);
+    
+}
