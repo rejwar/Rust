32739346cdfaa475e5c15d53d67102e0b53807b3
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-11 14:43:40 +0600

    Create I64EaxmplesFile.rs

---
+++ SignedInteger/I64EaxmplesFile.rs
@@ -0,0 +1,13 @@
+fn main(){
+    let Positivei64:i64 = 10000000000;
+    println!("Positive i64: {}", Positivei64);
+
+    let Negativei64:i64 = -100000000;
+    println!("Negative i64: {}", Negativei64);
+
+    let Maxi64:i64 = i64::MAX;
+    println!("Maxi64 :{}", Maxi64);
+
+    let Mini64:i64 = i64::MIN;
+    println!("Mini64 :{}", Mini64);
+}
