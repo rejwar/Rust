a4e89e253fdd49441e85e848adda95f591f86a37
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-11 11:34:20 +0600

    Create 1st.rs

---
+++ ScalarTypes/1st.rs
@@ -0,0 +1,17 @@
+fn main() 
+{
+    let Age: i32 = 25;
+
+    let Price: f64 = 19.55;
+
+    let Is_Available: bool = true;
+
+
+
+    println!("Age: {}", Age);
+    println!("Price: {}", Price);
+    println!("Is Avaiable: {}", Is_Available);
+}
+
+
+
