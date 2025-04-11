2720ff660a505c6ce74295f29c63b570c4977ffc
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-11 15:17:34 +0600

    Create U8ExampleFile.rs

---
+++ UnsignedInteger/U8ExampleFile.rs
@@ -0,0 +1,15 @@
+fn main() 
+{
+    let PositiveU8: u8 = 100;
+    println!("Positive U8: {}", PositiveU8);
+
+    let Maxu8: u8 = u8::MAX;
+    println!("Maximum U8: {}", Maxu8);
+
+    let Minu8: u8 = u8::MIN;
+    println!("Minimum U8: {}", Minu8);
+    
+
+
+
+}
