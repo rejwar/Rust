3e77710f7897b8ca753a4b39fd1c43a4e5732e91
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-11 12:12:45 +0600

    Create FloatArithmetic.rs

---
+++ FloatingTypes/FloatArithmetic.rs
@@ -0,0 +1,14 @@
+fn main()
+{
+    let Sum = 3.14+2.5;
+    let Product = 2.0 *3.5;
+    let Quotient =10.5/ 3.0;
+    let Remainder = 5.3 % 2.0;
+
+
+    println!("Sum : {}", Sum);
+    println!("Product:{}", Product);
+    println!("Quotient:{}", Quotient);
+    println!("REmainder :{}", Remainder);
+
+}
