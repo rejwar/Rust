ab232156209b63f9077d8e364f9ec7d5f87fa8b4
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-11 14:51:16 +0600

    Create I128ExampleFile.rs

---
+++ SignedInteger/I128ExampleFile.rs
@@ -0,0 +1,21 @@
+use std::num::ParseIntError;
+
+fn main()
+{
+    let Positivei128:i128 = 1000000000000000000;
+    println!("Positivei128: {}", Positivei128);
+    
+
+    let Neagativei128:i128 = -1000000000000000000;
+    println!("Negativei128: {}", Neagativei128);
+    
+
+    let Maxi128:i128 = i128::MAX;
+    println!("Maxi128: {}", Maxi128);
+    
+
+
+    let Mini128:i128 = i128::MIN;
+    println!("Mini128: {}", Mini128);
+    
+}
