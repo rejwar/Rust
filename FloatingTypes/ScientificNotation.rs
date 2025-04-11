6bf9a08830fdc8d1f376d2485a4104bf34c9dded
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-11 12:08:44 +0600

    Create ScientificNotation.rs

---
+++ FloatingTypes/ScientificNotation.rs
@@ -0,0 +1,8 @@
+fn main() 
+{
+    let Avogadro = 6.022e23;
+    let ElectronCharge = 1.6e-19;
+
+    println!("Avogrado Number:{}", Avogadro);
+    println!("Electron Charge: {}", ElectronCharge);
+}
