ce7d59b3b14bbef94dd165da46cdcd2d87f341f0
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-11 14:35:00 +0600

    Create I32ExampleFile.rs

---
+++ SignedInteger/I32ExampleFile.rs
@@ -0,0 +1,16 @@
+use std::i32;
+
+fn main()
+{
+    let Positivei32:i32 = 100000;
+    println!(" Positive i32:{}", Positivei32);
+
+    let Negativei32:i32 = -500000;
+    println!("Neagtive i32:{}", Negativei32);
+
+    let Maxi32:i32 = i32::MAX;
+    println!("Maximumi32 :{}", Maxi32);
+
+    let Min32:i32 = i32::MIN;
+    println!("Minimumi32 :{}", Min32);
+}
