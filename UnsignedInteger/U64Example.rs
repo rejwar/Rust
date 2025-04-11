fb4b635c494a57ebffc30bdf805eb77fb3fe5924
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-11 15:58:59 +0600

    Create U64Example.rs

---
+++ UnsignedInteger/U64Example.rs
@@ -0,0 +1,13 @@
+use std::u64;
+
+fn main() 
+{
+    let PositiveU64:u64 = 1000000000;
+    println!("PositiveU64: {}", PositiveU64);
+
+    let Maxu64:u64 = u64::MAX;
+    println!("Maxnumberu64: {}", Maxu64);
+
+    let Minu64:u64 = u64::MIN;
+    println!("Minimun u64: {}", Minu64);
+}
