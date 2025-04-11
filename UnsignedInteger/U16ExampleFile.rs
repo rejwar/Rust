138d014b54e5b2baf5f080c4e60ded5c57dfcd3a
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-11 15:23:45 +0600

    Create U16ExampleFile.rs

---
+++ UnsignedInteger/U16ExampleFile.rs
@@ -0,0 +1,12 @@
+ fn main() 
+{
+    let Positiveu16:u16 = 1000;
+    println!("Positive u16 {}", Positiveu16);
+
+
+    let Maximumu16:u16 = u16::MAX;
+    println!("Maximum Ui6{}", Maximumu16);
+
+    let Minimumu16:u16 = u16::MIN;
+    println!("Minimum u16{}", Minimumu16);
+}
