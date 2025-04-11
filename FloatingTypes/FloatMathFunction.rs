74fc0eeff1ca73989187223e6b112cbbb72e215f
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-11 12:36:34 +0600

    Create FloatMathFunction.rs

---
+++ FloatingTypes/FloatMathFunction.rs
@@ -0,0 +1,13 @@
+fn main()
+{
+
+    let x:f64 = 16.0;
+    let y: f32 = 2.0;
+
+    println!("Square root: {}", x.sqrt());
+    println!("Power(i32): {}", y.powi(3));
+    println!("Power (float): {}", y.powf(3.5));
+    println!("Exponential: {}", y.exp());
+    println!("Natuaral Log:{}", x.ln());
+}
+
