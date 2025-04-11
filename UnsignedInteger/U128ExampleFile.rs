a9b2235b2e08bb8b46f96fb36b458dc55a31c7fa
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-11 16:03:23 +0600

    Create U128ExampleFile.rs

---
+++ UnsignedInteger/U128ExampleFile.rs
@@ -0,0 +1,11 @@
+fn main() 
+{
+    let PositiveU128: u128 = 1000000000000000000;
+    println!("Positive u128 : {}", PositiveU128);
+
+    let MAXu128: u128 = u128::MAX;
+    println!("Maximum u128: {}", MAXu128);
+
+    let MinU128:u128 = u128::MIN;
+    println!("Minimum u128: {}" , MinU128);
+}
