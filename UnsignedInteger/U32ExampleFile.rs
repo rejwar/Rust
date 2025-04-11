19f5b349228321fa0f4ff33f43788ba8f8a6211f
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-11 15:56:00 +0600

    Create U32ExampleFile.rs

---
+++ UnsignedInteger/U32ExampleFile.rs
@@ -0,0 +1,13 @@
+fn main()
+{
+    let Positiveu32: u32 = 1000000;
+    println!("Positive U32: {}", Positiveu32);
+
+    let Maxu32: u32 = u32::MAX;
+    println!("Maximumu32 {}", Maxu32);
+
+    let Min32:u32 = u32::MIN;
+    println!("Minimumu32{}", Min32);
+
+    
+}
