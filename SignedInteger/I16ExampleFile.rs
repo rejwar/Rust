416d02f452145c308971ba5f3106eff55b916eee
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-11 14:23:51 +0600

    Create I16ExampleFile.rs

---
+++ SignedInteger/I16ExampleFile.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let PositiveI16:i16 = 1000;
+    println!("Positive i16: {}", PositiveI16);
+
+    let NegativeI16:i16 = -5000;
+    println!("Negative i16: {}", NegativeI16);
+
+    let MaxI16:i16 = i16::MAX;
+    println!("Maximum i16: {}", MaxI16);
+
+    let MinI16:i16 = i16::MIN;
+    println!("Minimum i16{}", MinI16);
+}
