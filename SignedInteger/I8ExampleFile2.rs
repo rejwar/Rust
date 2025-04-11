88bd31a9ff3c250d74e301b69c6635c3cbcc943c
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-11 14:07:34 +0600

    Create I8ExampleFile2.rs

---
+++ SignedInteger/I8ExampleFile2.rs
@@ -0,0 +1,14 @@
+fn main() 
+{
+    let PositiveI8: i8 = 100;
+    println!("Positive i8: {}", PositiveI8);
+
+    let NegativeI8 : i8 = -50;
+    println!("Negative i8: {}", NegativeI8);
+
+    let MaxI8:i8 = i8::MAX;
+    println!("Maximum I8:{}", MaxI8);
+
+    let MinI8:i8 = i8::MIN;
+    println!("Minimum i8:{}", MinI8);
+}
