5af4f4066b7b7c8cf5b108102080168710a70ee5
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-12 20:51:50 +0600

    Create LoopExitBoolean.rs

---
+++ Boolean/LoopExitBoolean.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let mut IsProcessing: bool = true;
+
+    while IsProcessing {
+        println!("Processing...");
+        IsProcessing = false; // Exit condition
+    }
+
+    println!("Processing Finished!");
+}
