e854dff65ca1da1907745d2e2a83542864d949bd
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-13 02:45:31 +0600

    Create CharDeclare.rs

---
+++ Character/CharDeclare.rs
@@ -0,0 +1,14 @@
+fn DeclareChar() 
+{
+    let letter:char = 'A';
+    let digit:char = '7';
+    let symbol:char = '$';
+    let emoji:char = '😀';
+
+    println!("Char values:{} ,{},{},{}",letter,digit,symbol,emoji);
+}
+
+fn main()
+{
+    DeclareChar();
+}
