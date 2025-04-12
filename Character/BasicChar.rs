aa43c5f6cccee1df628acc02055967b98e052cb8
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-13 01:47:53 +0600

    Create BasicChar.rs

---
+++ Character/BasicChar.rs
@@ -0,0 +1,17 @@
+fn main()
+{
+    let letter:char = 'a';
+    let digit:char = '1';
+    let bangla:char = 'ক';
+    let emoji:char = '🙂';
+    let space:char = ' ';
+    let newLine:char = '\n';
+
+    println!("Letter : {}",letter);
+    println!("Digit : {}",digit);
+    println!("Bangla :{}", bangla);
+    println!("Emoji : {}", emoji);
+    println!("Space: {}",space);
+    println!("newLine: {}",newLine);
+    println!("After Newline.");
+}
