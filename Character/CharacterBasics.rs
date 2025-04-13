72466be80f970cf36262f38539bfc9de81b957cc
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-13 11:41:14 +0600

    Create CharacterBasics.rs

---
+++ Character/CharacterBasics.rs
@@ -0,0 +1,11 @@
+fn main()
+{
+    let SingleChar:char = 'A';
+    println!("Character : {}", SingleChar);
+
+    let BanglaChar:char = 'ক';
+    let EmojiChar:char = '😃';
+
+    println!("BanglaCharacter {}", BanglaChar);
+    println!("EmojiChar {}",EmojiChar);
+}
