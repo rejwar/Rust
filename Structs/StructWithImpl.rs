3aecc51294ccf44081febbcc37c63444ba982406
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-14 12:31:42 +0600

    Create StructWithImpl.rs

---
+++ Structs/StructWithImpl.rs
@@ -0,0 +1,17 @@
+struct  Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn Area (&self) -> u32 {
+        self.width * self.height
+    }
+}
+
+fn main() {
+    let rect = Rectangle { width: 10 , height:5};
+    {
+        println!("Area: {}", rect.Area());
+    }
+}
