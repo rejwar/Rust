e7a9f9fc327c1b998c3757b90bfd3d474dbfccc3
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-14 11:32:30 +0600

    Create BasicArrayExample.rs

---
+++ Array/BasicArrayExample.rs
@@ -0,0 +1,9 @@
+fn BasicArrayExample() {
+    let Numbers = [10,20,30,40];
+    println!("First: {}",Numbers[0]);
+    println!("Third :{}",Numbers[2]);
+}
+
+fn main() {
+    BasicArrayExample();
+}
