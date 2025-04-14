3ad5f0a670252f100f3b8b514b5dffaaf83d2916
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-14 16:50:27 +0600

    Create OptionEnum1.rs

---
+++ Enum/OptionEnum1.rs
@@ -0,0 +1,16 @@
+fn FindIndex(value: i32 , arr:&[i32]) -> Option<usize> {
+    for (i, &v) in arr.iter().enumerate(){
+        if v ==value{
+            return Some(i);
+        }
+    }
+    None
+}
+
+fn main() {
+    let nums = [3,5,7];
+    match FindIndex(5,&nums ){
+        Some(index) => println!("Found at {}" , index),
+        None =>println!("Not found"),
+    }
+}
