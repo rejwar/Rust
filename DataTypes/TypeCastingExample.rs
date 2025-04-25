39ba5af5affb7d952d094bcc343a43f009c5d999
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-26 05:34:54 +0600

    Create TypeCastingExample.rs

---
+++ DataTypes/TypeCastingExample.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let MileyAway: i32 = 50;
+    let MilesAway: i8 = MileyAway as i8;
+    println!("MilesAway: {}", MilesAway);
+    let MilesAway: i8 = 50;
+    let MileyAway: i32 = MilesAway as i32;
+    println!("MileyAway: {}", MileyAway);
+    let MilesAway: i8 = 50;
+    let MileyAway: i32 = MilesAway as i32;
+    println!("MileyAway: {}", MileyAway);
+    let MileyAway: i32 = 50;
+    let MilesAway: i8 = MileyAway as i8;
+    println!("MilesAway: {}", MilesAway);
+    let MilesAway: i8 = 50;
+    let MileyAway: i32 = MilesAway as i32;
+    println!("MileyAway: {}", MileyAway);
+}
