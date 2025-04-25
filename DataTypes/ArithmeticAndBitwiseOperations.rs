7a99ee2a13514db499bd43df925fd1dedf67e18d
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-26 05:43:20 +0600

    Create ArithmeticAndBitwiseOperations.rs

---
+++ DataTypes/ArithmeticAndBitwiseOperations.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let additional = 5 + 4;
+
+    let subtraction: i32 = 10 -6;
+
+    let multiplication: i32 = 2 * 3;
+
+    let division: i32 = 10 / 2;
+
+    let modulo: i32 = 10 % 3;
+
+    let bitwise_and: i32 = 5 & 3;
+}
