ab5d91a0d267c615d7e2d7b082d1fe0b418a0859
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-26 05:20:44 +0600

    Create FloatingPointOperations.rs

---
+++ DataTypes/FloatingPointOperations.rs
@@ -0,0 +1,30 @@
+fn main() {
+    let pi:f64 = 3.14159265358979323846;
+    println!("Pi is approximately: {}", pi);
+
+    println!("{}", pi.floor());
+    println!("{}", pi.ceil());
+    println!("{}", pi.round());
+    println!("{}", pi.trunc());
+    println!("{}", pi.fract());
+    println!("{}", pi.abs());
+    println!("{}", pi.sqrt());
+    println!("{}", pi.sin());
+    println!("{}", pi.cos());
+    println!("{}", pi.tan());
+    println!("{}", pi.asin());
+    println!("{}", pi.acos());
+    println!("{}", pi.atan());
+    println!("{}", pi.atan2(1.0));
+    println!("{}", pi.exp());
+    println!("{}", pi.ln());
+    println!("{}", pi.log(2.0));
+    println!("{}", pi.log(10.0));
+    println!("{}", pi.log(3.0));
+    println!("{}", pi.log(5.0));
+    println!("{}", pi.log(7.0));
+    println!("{}", pi.log(11.0));
+    println!("{}", pi.log(13.0));
+    println!("{}", pi.log(17.0));
+    println!("{}", pi.log(19.0));
+}
