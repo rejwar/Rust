c5609f70d74eb01867d43b407d3ad66f49d8afea
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-26 04:57:16 +0600

    Create Hi.rs

---
+++ PrimitiveTypes/Hi.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("Dear Emily , \n How have u been? I hope you are doing well. I am writing to you to let you know that I have been thinking about you a lot lately. I miss our conversations and the time we spent together. I hope we can catch up soon. \n Best wishes, \n Your friend");
+    println!("\t Once upon a time");
+    println!("Juliat said \"I love you\" to her boyfriend\" ");
+    let filepath = "src/main.rs";
+    println!("{}", filepath);
+    
+}
