b981edf58dc232233d6bc01811cb8eceb975f353
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-26 05:48:50 +0600

    Create IncrementVariables.rs

---
+++ DataTypes/IncrementVariables.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let mut  year:i32 =  2023;
+
+    year = year + 1;
+    println!("The year is: {}", year);
+    let mut  month:i32 =  10;
+    month = month + 1;
+    println!("The month is: {}", month);
+    let mut  day:i32 =  1;
+    day = day + 1;
+    println!("The day is: {}", day);
+}
