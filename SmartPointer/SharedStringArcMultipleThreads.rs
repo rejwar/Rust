1970ecc9d7f132a1d5dc11bae4358bff571c6a42
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-26 17:26:22 +0600

    Create SharedStringArcMultipleThreads.rs

---
+++ SmartPointer/SharedStringArcMultipleThreads.rs
@@ -0,0 +1,26 @@
+use std::sync::Arc;
+use std::thread::{self, Thread};
+
+fn main() {
+    let ShareData: Arc<String> = Arc::new(String::from("Concurrency"));
+
+    let ThreadOne = {
+        let CloneData = Arc::clone(&ShareData);
+        thread::spawn(move || {
+            println!("Thread One: {}", CloneData);
+        }) 
+    };
+
+    let ThreadTwo = {
+        let CloneData  = Arc::clone(&ShareData);
+        thread::spawn(move || {
+            println!("Thread Two: {}", CloneData);
+        })
+    };
+
+
+    ThreadOne.join().unwrap();
+    ThreadTwo.join().unwrap();
+    println!("Main Thread: {}", ShareData);
+    println!("Main Thread: {}", ShareData);
+}
