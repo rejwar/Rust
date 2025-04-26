69fe326ca11a7704639cb724923ab1e60770dc3c
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-26 06:21:30 +0600

    Create LogicalNotAndComparison.rs

---
+++ DataTypes/LogicalNotAndComparison.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("{}" , !true);
+    println!("{}" , !false);
+
+    let age:i32 = 13;
+    let can_see_rated_r_movie:bool = age >= 17;
+    println!("{}" , can_see_rated_r_movie);
+}
