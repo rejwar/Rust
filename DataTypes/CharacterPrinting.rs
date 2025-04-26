c8c78f7d0be92a4e4c421cbcd1369b9867be8037
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-26 07:58:04 +0600

    Create CharacterPrinting.rs

---
+++ DataTypes/CharacterPrinting.rs
@@ -0,0 +1,24 @@
+fn main() {
+    let FirstInitail: char = 'A';
+    let SecondInitail: char = 'B';
+    let ThirdInitail: char = 'C';
+    let FourthInitail: char = 'D';
+    let FifthInitail: char = 'E';
+    let SixthInitail: char = 'F';
+    let SeventhInitail: char = 'G';
+    let EighthInitail: char = 'H';
+    let Example: &str = "Hello, world!";
+
+    let emoji:char = "😀" ;
+
+    println!(
+        " "{}" ,"{}" , "{}", "{}", "{}",
+        "{}"" ,
+        FirstInitail,
+        SecondInitail,
+        ThirdInitail,
+        FourthInitail,
+        FifthInitail,
+        SixthInitail,
+    )
+}
