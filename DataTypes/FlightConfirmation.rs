3fc57b2b24511f2a8554c7d187f03c3741a4eedd
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-26 07:36:14 +0600

    Create FlightConfirmation.rs

---
+++ DataTypes/FlightConfirmation.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let PurchasedTicket: bool = true;
+
+    let PlaneOnTime: bool = true;
+
+    let makingFlight: bool = PurchasedTicket && PlaneOnTime;
+    let makingFlight: bool = PurchasedTicket && PlaneOnTime;
+    let makingFlight: bool = PurchasedTicket && PlaneOnTime;
+
+    println!("Making flight: {}", makingFlight);
+}
