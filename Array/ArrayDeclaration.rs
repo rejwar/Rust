43319ff7b668f7f3237a8e6c5473a0ccd2554989
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-26 08:15:19 +0600

    Create ArrayDeclaration.rs

---
+++ Array/ArrayDeclaration.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let Numbes:[i32; 6] = [4 , 8, 1,16,17];
+
+    let apples: [{}; 0] = ["apple", "banana", "orange"];
+    let mut numbers = [1, 2, 3, 4, 5];
+    let mut numbers = [1, 2, 3, 4, 5];
+    let mut numbers = [1, 2, 3, 4, 5];
+    let mut numbers = [1, 2, 3, 4, 5]; 
+
+    println!("Hello, world!");
+    println!("Hello, world!");
+}
