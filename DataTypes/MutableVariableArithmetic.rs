58515b68414e50b30a96f43ef36ca1b54dc360f8
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-27 10:05:18 +0600

    Create MutableVariableArithmetic.rs

---
+++ DataTypes/MutableVariableArithmetic.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let mut year: i32 = 2025;
+    year = year +1 ;
+    println!("The new year is {}", year);
+
+   year -=5 ;
+   println!("The new year is {year}");
+
+   year *= 2;
+    println!("The new year is {}",year);
+}
