0b73d82c4598b756b50ca97141321a80a32724d8
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-27 09:58:20 +0600

    Create NumericTypeCasting.rs

---
+++ DataTypes/NumericTypeCasting.rs
@@ -0,0 +1,11 @@
+use std::iter::MapWhile;
+
+fn main() {
+    let miles_away:i32 = 50;
+    let miles_away_i8: i8 = miles_away as i8;
+    let miles_away_i8: u8 = miles_away_i8 as u8;
+
+    let miles_away:f64 = 100.32343;
+    let miles_away_f32:f32 = miles_away as f32;
+    let miles_away_int:i32 = miles_away as i32;
+}
