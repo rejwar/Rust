e44b920b35f81efa864d85bec64e59ceb487a36d
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-27 20:07:45 +0600

    Create PizzaStoreSimulation.rs

---
+++ Function/PizzaStoreSimulation.rs
@@ -0,0 +1,16 @@
+fn main() {
+    open_store();
+}
+
+fn open_store() {
+    println!("Opening my pizza store");
+}
+
+fn bake_pizza  () {
+    println!("Baking pizza ");
+
+}
+
+fn swim_in_profit() {
+    println!("So much $$$ , so little time");
+}
