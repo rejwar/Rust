4f8ac2266c1274329180453382a4ce210970f23b
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-27 14:15:16 +0600

    Create MutableStringArrayWithDebug.rs

---
+++ DataTypes/MutableStringArrayWithDebug.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let mut seasons : [&str; 4] = ["Spring" , "Summer" ,"Fall" , "Winter"];
+
+    println!("{}", seasons[2]);
+    seasons[2] = "Autumn";
+    println!("{}" , seasons[2]);
+    println!("{}" ,true);
+    println!("{:#?}",seasons);
+
+    dbg!(2 +2 );
+    dbg!(seasons);
+}
