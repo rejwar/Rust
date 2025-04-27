72610cacc2ebcb0a5c5691a422d09ab899bcd551
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-27 18:10:59 +0600

    Create ExclusiveInclusiveRange.rs

---
+++ DataTypes/ExclusiveInclusiveRange.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let MyRange = 1..5;
+    let InclusiveRange = 1..=5;
+
+    println!("Exclusive Range : {:?}", MyRange);
+
+    println!("Inclusive Range : {:?}" , InclusiveRange);
+}
