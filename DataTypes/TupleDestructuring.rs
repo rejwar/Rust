113998d721916c961373bd8e51e87ccc19a6baf7
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-27 14:23:54 +0600

    Create TupleDestructuring.rs

---
+++ DataTypes/TupleDestructuring.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let employee=("Molly" , 32 , "Marketing");
+
+    let name = employee.0;
+    let age = employee.1;
+    let department = employee.2;
+    println!("Name: {name} , age: {age} , department: {department}");
+
+
+
+
+
+    println!("{employee:#?}")
+}
