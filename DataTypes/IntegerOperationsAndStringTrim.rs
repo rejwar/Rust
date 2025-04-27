05dcdd847426826f805b74d7b8e8954ebb947df1
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-27 09:30:34 +0600

    Create IntegerOperationsAndStringTrim.rs

---
+++ DataTypes/IntegerOperationsAndStringTrim.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let value: i32 = -15;
+    println!("{}", value.abs());
+
+    let empaty_space:&str = "   My content";
+    println!("{}" ,empaty_space.trim());
+
+    println!("{}", value.pow(2));
+    println!("{}" , value.pow(3));
+
+}
