87e6dcd042777c61ddc146c13711ca6bfdeafb68
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-28 11:29:31 +0600

    Create ConditionalSeasonCheck.rs

---
+++ ControlFlow/ConditionalSeasonCheck.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let season = "summer";
+
+    if season == "summer" {
+        println!("School is out ");
+    } else if season == "winter" {
+        println!("Brr , so cold");
+    } else if season == "fall" {
+        println!("Leaves falling");
+    }else  if season == "spring"{
+        println!("Lots of rain");
+        
+    }
+}
