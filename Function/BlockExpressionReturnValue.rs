bc5a184db840c5e9cfa3f8bf9dd8b79c29f0be2e
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-28 07:05:13 +0600

    Create BlockExpressionReturnValue.rs

---
+++ Function/BlockExpressionReturnValue.rs
@@ -0,0 +1,15 @@
+
+fn CalculateSum() -> i32  {
+
+    let Result = {
+        let a = 5;
+        let b: i32 = 6;
+
+        a + b
+    };
+    Result
+}
+
+fn main() {
+    println!("Sum is :{}" , CalculateSum());
+}
