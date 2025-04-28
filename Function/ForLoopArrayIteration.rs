5a15dfb857b42dc408f6c3843aee124d99902396
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-28 08:40:08 +0600

    Create ForLoopArrayIteration.rs

---
+++ Function/ForLoopArrayIteration.rs
@@ -0,0 +1,13 @@
+use std::array;
+
+fn ForLoopExample () {
+    let array= [10,20,30,40,50];
+
+    for element in array {
+        println!("Element : {}" , element);
+    }
+}
+
+fn main() {
+    ForLoopExample();
+}
