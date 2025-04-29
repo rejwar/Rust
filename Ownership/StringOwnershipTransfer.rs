63aa6d8d7be0c977e4e66463132050a2f2ad9147
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-29 06:30:30 +0600

    Create StringOwnershipTransfer.rs

---
+++ Ownership/StringOwnershipTransfer.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let person:String = String::from("Boris");
+    println!("My name is {}",person);
+    let genius:String = person;
+
+    
+
+    //println!("My Name is {}",person);
+}
