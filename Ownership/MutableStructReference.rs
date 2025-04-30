9107d2948857a455d1ea2fb5c8b800b2c5093a23
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-30 08:13:44 +0600

    Create MutableStructReference.rs

---
+++ Ownership/MutableStructReference.rs
@@ -0,0 +1,13 @@
+struct Counter {
+    Value: i32,
+}
+
+fn Increment (Counter: &mut Counter) {
+    Counter.Value +=1;
+}
+
+fn main() {
+    let mut MyCounter: Counter = Counter { Value: 0 };
+    Increment(&mut MyCounter);
+    println!("Updated Counter {}", MyCounter.Value);
+}
