4bc25a02b1585f54b50f0dda653a6d498dd69bc3
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-05-04 05:34:07 +0600

    Create HashMapAccessingNonExistentKey.rs

---
+++ HashMap/HashMapAccessingNonExistentKey.rs
@@ -0,0 +1,12 @@
+use std::collections::{btree_map::Values, HashMap};
+
+fn main() {
+    let mut CoffeePairing: HashMap<&str , &str> = HashMap::new();
+    let Drink = String::from("Latte");
+    let Milk= String::from("Oat Milk");
+    CoffeePairing.insert(&Drink, &Milk);
+    CoffeePairing.insert("Flat White", "Almond Milk");
+
+    let Values = CoffeePairing["Cappuccino"];
+    println!("{}" , Values);
+}
