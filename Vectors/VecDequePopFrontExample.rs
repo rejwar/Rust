354444f98eb571f1a3763b03b05d317f9cfc2ff0
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-05-03 07:09:05 +0600

    Create VecDequePopFrontExample.rs

---
+++ Vectors/VecDequePopFrontExample.rs
@@ -0,0 +1,7 @@
+use std::collections::VecDeque;
+
+fn main() {
+    let mut Queue: VecDeque<i32> = VecDeque::front(vec![10,20,30]);
+    Queue.pop_front();
+    println!("Updated Queue {:?}", Queue);
+}
