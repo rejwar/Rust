a6beb8ac4af367d002723206ca28379357251177
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-05-16 06:24:07 +0600

    Create StringBorrowing.rs

---
+++ Ownership/StringBorrowing.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let s = String::from("Hello");
+    let len = calculate_length(&s);
+    println!("The length of '{}' is {}.", s, len);
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
