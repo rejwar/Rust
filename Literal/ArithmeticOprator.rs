e7638c580864defe72187c6db34178feb44b6550
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-05-16 12:12:28 +0600

    Create ArithmeticOprator.rs

---
+++ Literal/ArithmeticOprator.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let A = 10;
+    let B = 3;
+
+    println!("Add : {}" , A + B);
+    println!("Subtract : {}" , A -B);
+    println!("Multiply : {}" , A *B);
+    println!("Division : {}" , A / B);
+    
+}
