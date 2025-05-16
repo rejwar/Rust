78a34ebe29149caf3daed8d984253e3cdfeb5956
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-05-16 11:51:41 +0600

    Create IntegerLiterals.rs

---
+++ Literal/IntegerLiterals.rs
@@ -0,0 +1,10 @@
+use std::fmt::Binary;
+
+fn main() {
+    let Decimal = 10;
+    let Hex = 0xA;
+    let Octal = 0o12;
+    let Binary = 0b1010;
+
+    println!("Decimal : {} , Hex : {} , Octal : {} , Binary : {}" , Decimal, Hex , Octal , Binary);
+}
