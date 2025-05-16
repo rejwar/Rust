dcd93be9cefe162dee071908c234a918421fe14c
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-05-16 12:34:37 +0600

    Create LogicalOperators.rs

---
+++ Literal/LogicalOperators.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let TrueValue = true;
+    let FlaseValue = false;
+
+    println!("AND: {}" , TrueValue && FlaseValue);
+    println!("OR : {}" , TrueValue || FlaseValue);
+    println!("NOT : {}" , !TrueValue);
+}
