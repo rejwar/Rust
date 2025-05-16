6be971a601ea9beec6349aca2e5502a5122de8bd
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-05-16 12:16:06 +0600

    Create ComparisonOperator.rs

---
+++ Literal/ComparisonOperator.rs
@@ -0,0 +1,11 @@
+fn main(){
+    let X = 5;
+    let Y = 10;
+
+    println!("Equal {}" , X ==Y);
+    println!("Not Equal : {}" , X != Y);
+    println!("Less Than {}" , X < Y);
+    println!("Greater Than {}" , X>Y);
+    println!("Less or Equal {}" , X <=Y);
+    println!("Greater or Equal {}" , X>= Y);
+}
