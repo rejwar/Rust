bd4943088c03fbfec746632ae6393fded500970b
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-05-18 02:46:59 +0600

    Create Program.rs

---
+++ Tuple/Program.rs
@@ -0,0 +1,13 @@
+// fn main() {
+//     let N:usize = 20;
+//     let arr = [0; N];
+//     print!("{}", arr[10])
+// }
+
+
+fn main() {
+    const N: usize = 20;
+    let arr = [0;N];
+
+    print!("{}", arr[10]);
+}
