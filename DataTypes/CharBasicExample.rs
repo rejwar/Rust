1cf03e99fcebb34dba6ef5015a9b430eb22b8ccb
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-05-17 13:56:09 +0600

    Create CharBasicExample.rs

---
+++ DataTypes/CharBasicExample.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let specail_character = '@';
+    let alphabate:char = 'A';
+    let emoji:char = '';
+
+    println!("specail character is {}", specail_character);
+    println!("alphabate is {}", alphabate);
+    println!("emoji is {}", emoji);
+}
