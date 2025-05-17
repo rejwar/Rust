15f1a4d9b797af29d944e01a93b08309b41fb3f6
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-05-18 01:59:50 +0600

    Create Tuple.rs

---
+++ Tuple/Tuple.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let tuple:(i32 , f64, u8) = (-325, 4.9,22);
+    println!("interger is : {:?}", tuple.0);
+    println!("float is : {:?}",tuple.1);
+    println!("insigned integer is : {:?}",tuple.2);
+}
