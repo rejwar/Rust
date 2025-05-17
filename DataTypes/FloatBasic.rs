b3dfb87a2f7cf285ba619a505b0f27f0242f2f53
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-05-17 13:49:59 +0600

    Create FloatBasic.rs

---
+++ DataTypes/FloatBasic.rs
@@ -0,0 +1,11 @@
+use std::result;
+
+fn main() {
+    let Result = 10.0;
+    let interest= 8.32;
+    let cost = 150000.600;
+
+    println!("result is {}", Result );
+    println!("Interest is {}" , interest);
+    println!("Cost is {}" , cost);
+}
