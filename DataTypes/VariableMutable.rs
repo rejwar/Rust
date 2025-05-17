5e7bdcb1144d378cb4ec22fed73c04d3710380a4
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-05-17 14:05:52 +0600

    Create VariableMutable.rs

---
+++ DataTypes/VariableMutable.rs
@@ -0,0 +1,10 @@
+let mut VariableName= value;
+let mut VariableName = value;
+
+fn main() {
+    let mut fees:i32 = 25_000;
+    println!("Fees is {}" , fees);
+
+    fees = 35_000;
+    println!("Fees changeed is {}" , fees);
+}
