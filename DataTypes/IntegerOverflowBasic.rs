c9a6152576d7805ae06c8dbd3e276d5254b94106
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-05-17 13:46:05 +0600

    Create IntegerOverflowBasic.rs

---
+++ DataTypes/IntegerOverflowBasic.rs
@@ -0,0 +1,13 @@
+
+fn main() {
+    let age:u8 = 255;
+
+    let weight:u8 = 256;
+    let height:u8 = 257;
+    let score:u8 = 258;
+
+    println!("Age is {} " , age);
+    println!("Weight is {}" , weight);
+    println!("Height is {}", height);
+    println!("Score is {}", score );
+}
