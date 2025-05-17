4e64a7a226a8910262e038507e265c1003dc9f25
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-05-17 13:41:42 +0600

    Create New.rs

---
+++ DataTypes/New.rs
@@ -0,0 +1,13 @@
+use std::result;
+
+fn main() {
+    let result = 10;
+    let age:u32 = 20;
+    let sum:i32 = 5-15;
+    let mark:isize = 10;
+    let count:usize = 30;
+
+    println!("Result value , {}", age);
+    println!("sum is {}" , sum);
+    println!("Mark is {} and count is {}" , mark,count);
+}
