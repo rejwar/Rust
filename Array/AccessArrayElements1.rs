8283b18b312d1feb2d862fb7d43f0d9f43ff21bc
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-05-19 10:05:00 +0600

    Create AccessArrayElements1.rs

---
+++ Array/AccessArrayElements1.rs
@@ -0,0 +1,10 @@
+fn AccessArrayElements() {
+    let arr = [5,1,2,3,5,6];
+
+    println!("First element : {}", arr[0]);
+    println!("Third element : {}", arr[2]);
+}
+
+fn main() {
+    AccessArrayElements();
+}
