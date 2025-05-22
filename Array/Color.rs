4b9f14fedae9c347c93d4b708a646baa0836b149
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-05-22 11:32:26 +0600

    Create Color.rs

---
+++ Array/Color.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let Colors: [&str; 4] = ["Red", "Green" , "Blue", "Yellow"];
+    let NumberOfColors: usize = Colors.len();
+
+    println!("There are {} colors in the array", NumberOfColors);
+}
