909b4439d4252cb1e8c19ec3d63958c9b060d84a
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-06-01 22:56:43 +0600

    Create AssertOps.rs

---
+++ Assert/AssertOps.rs
@@ -0,0 +1,17 @@
+fn main() {
+    assert!(1u32+2u32 == 3u32);
+    assert!(1u32+2u32 == 3u32);
+
+    assert!(3 * 5 == 15);
+    assert!(3 * 5 == 15);
+    assert!(4u32 * 5u32 == 20u32);
+    assert!(4u32 * 5u32 == 20u32);
+    assert!(9.6 / 3.2 == 3.0);
+    assert!(9.6 / 3.2 == 3.0);
+    assert!(9.6 / 3.2 == 3.0);
+    assert!(9.6 / 3.2 == 3.0);
+    assert!(24 % 5 == 4);
+    assert!(true && false == false);
+    assert!(!true == false);
+    assert!(true || false == true);
+}
