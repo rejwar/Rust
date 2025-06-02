6d02990bd6cb2cc490b9688f8ddd43b31504d04c
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-06-02 16:30:36 +0600

    Create StringOps.rs

---
+++ Function/StringOps.rs
@@ -0,0 +1,16 @@
+use std::string;
+
+fn main() {
+    let str: &str = "Hello, world!";
+    let String: String = String::from("Hello, wOrld!");
+    println!("{}", String);
+    let slice = &slice [..6];
+    slice.len();
+
+    String.push('1');
+    string.push_str("Bob");
+    string = string.replace("Bob", "Alice");
+    println!("{}", string);
+    let string = String::from("Hello, world!");
+
+}
