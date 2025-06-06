77536cf5eda9367a7a822eaa4dcfcb743a52153e
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-06-07 01:54:31 +0600

    Create ExpressionReturnExample.rs

---
+++ Arrow/ExpressionReturnExample.rs
@@ -0,0 +1,8 @@
+fn Multiply(Num1: i32, Num2: i32) -> i32 {
+    Num1 * Num2 // এক্সপ্রেশন রিটার্ন করা
+}
+
+fn main() {
+    let Product = Multiply(5, 4);
+    println!("Product: {}", Product); // ফলাফল: 20
+}
