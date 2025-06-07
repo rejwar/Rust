1398ea41657ac07512ee27e4927b28e693aabf2b
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-06-07 16:34:29 +0600

    Create Vec.rs

---
+++ Arrow/Vec.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let vec1 = vec![1, 2, 3];
+    let vec2 = vec![4, 5, 6];
+    let vec3 = vec![7, 8, 9];
+
+    println!("Vector 1: {:?}", vec1);
+    println!("Vector 2: {:?}", vec2);
+    println!("Vector 3: {:?}", vec3);
+}
