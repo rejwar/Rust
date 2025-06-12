9129c5437cb2289a177d684f5cf4f4798a5240cb
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-06-12 14:13:39 +0600

    Create InfiniteBrea1k.rs

---
+++ The Rust Programming Language Book/Chapter 3/InfiniteBrea1k.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let mut Count = 0;
+
+    while true {
+        if Count >= 3 {
+            break;
+        }
+
+        println!("Loop Count: {}", Count);
+        Count += 1;
+    }
+
+    println!("Loop terminated manually.");
+}
