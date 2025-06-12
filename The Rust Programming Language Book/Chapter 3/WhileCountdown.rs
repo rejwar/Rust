c1632b3dd8c8317b32c23bb6ec0937060f110bd5
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-06-12 12:28:45 +0600

    Create WhileCountdown.rs

---
+++ The Rust Programming Language Book/Chapter 3/WhileCountdown.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let mut Counter:i32=5;
+
+    while Counter >0 {
+        println!("Countdown {}", Counter);
+        Counter -=1;
+
+    }
+    println!("Life off ! {}" , Counter);
+}
