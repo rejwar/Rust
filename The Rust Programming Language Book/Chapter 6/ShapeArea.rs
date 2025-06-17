a841e5417cabe4633dc403d6c32b821d41ef9e1e
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-06-17 17:08:06 +0600

    Create ShapeArea.rs

---
+++ The Rust Programming Language Book/Chapter 6/ShapeArea.rs
@@ -0,0 +1,13 @@
+enum Shape {
+    Circle(f64),
+    Rectangle(f64, f64),
+}
+
+impl Shape {
+    fn area(&self) -> f64 {
+        match self {
+            Shape::Circle(r) => std::f64::consts::PI * r * r,
+            Shape::Rectangle(w, h) => w * h,
+        }
+    }
+}
