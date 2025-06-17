7704f153c1b4ceb7cb2a5084398398757b489f0a
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-06-17 12:09:37 +0600

    Create IfLetEnumExample.rs

---
+++ The Rust Programming Language Book/Chapter 6/IfLetEnumExample.rs
@@ -0,0 +1,9 @@
+// Question: How can we use `if let` to simplify pattern matching?
+
+fn UseIfLet() {
+    let value = Some("Rust");
+
+    if let Some(lang) = value {
+        println!("You're learning {}", lang);
+    }
+}
