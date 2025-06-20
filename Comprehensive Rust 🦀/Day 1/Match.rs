1cb84c11d6d633fc430caa7cb86f10c3a21e1167
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-06-21 03:10:26 +0600

    Create Match.rs

---
+++ Comprehensive Rust 🦀/Day 1/Match.rs
@@ -0,0 +1,12 @@
+use core::num;
+
+fn main() {
+    let number =3;
+
+    match number {
+        1 => println!("One"),
+        2 => println!("Two"),
+        3 => println!("Three"),
+        _=> println!("Others"),
+    }
+}
