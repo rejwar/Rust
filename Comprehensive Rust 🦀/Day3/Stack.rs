d99fc7990a9dfe3c88c06616fc41dd91feb70614
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-06-27 03:36:16 +0600

    Create Stack.rs

---
+++ Comprehensive Rust 🦀/Day3/Stack.rs
@@ -0,0 +1,19 @@
+use std::array;
+
+fn main() {
+    let number =42;
+    let decimal = 3.14;
+    let letter = "a";
+    let is_true = true;
+    
+
+    println!(" Number : {}", number);
+    println!(" Decimal is {}", decimal);
+    println!(" letter is {}",letter);
+    println!(" Boolean is {}", is_true);
+
+
+
+    let array = [1,2,3,4,5,6,7];
+    println!("Print array : {:?}",array);
+}
