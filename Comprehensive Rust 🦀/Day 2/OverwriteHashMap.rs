4d882e21d9123437ac24ca34f6060eb59e109f2b
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-06-26 13:01:10 +0600

    Create OverwriteHashMap.rs
    Question: What happens when you insert a value with an existing key?

---
+++ Comprehensive Rust 🦀/Day 2/OverwriteHashMap.rs
@@ -0,0 +1,11 @@
+use std::collections::HashMap;
+
+
+
+fn main() {
+    let mut map = HashMap::new();
+    map.insert("Alice", "Bob");
+    map.insert("Token", "xyz456");
+
+    println!("{:?}",map);c
+}
