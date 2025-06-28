d3689e0ee38f226111d2bbea1c0fcd710c672d57
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-06-28 12:28:55 +0600

    Create 1RustModulePrivacyExample.rs

---
+++ Comprehensive Rust 🦀/Day4/1RustModulePrivacyExample.rs
@@ -0,0 +1,13 @@
+// nested module
+pub mod player {
+    pub mod sprite {
+        pub fn create() {
+            println!("called player::sprite::create");
+        }
+    }
+}
+
+fn main() {
+    // call public function create from sprite module which is inside player module 
+    player::sprite::create();
+}
