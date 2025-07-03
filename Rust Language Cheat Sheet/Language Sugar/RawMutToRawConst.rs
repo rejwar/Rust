6ee1382e4fee1cbb6e9a71ab742fa7a9dce3fc9f
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-07-03 14:45:52 +0600

    Create RawMutToRawConst.rs

---
+++ Rust Language Cheat Sheet/Language Sugar/RawMutToRawConst.rs
@@ -0,0 +1,11 @@
+
+
+fn main() {
+    let mut value = 5;
+    let mut_ptr: *mut i32 = &mut value;
+    let const_ptr: *const i32 = mut_ptr;
+
+    unsafe  {
+        println!("Value is {}", *const_ptr)
+    }
+}
