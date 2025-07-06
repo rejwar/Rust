bdee0f6be4f289dc7708019254539f7433ac99b5
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-07-06 14:23:14 +0600

    Create RustNumberLiterals.rs

---
+++ Rust By Example/Types/RustNumberLiterals.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let decimal = 42;
+    let hex = 0x2A;
+    let octal = 0o101010;
+    let binary = 0b10101;
+    let byte = b'A';
+
+    println!("Decimal : {} , Hex{} , Octal {} , binary {} byte: {} ", decimal, hex, octal , binary, byte);
+
+}
