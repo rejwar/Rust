aa77621650673098c38dba8796e8ed2c60b658bc
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-07-06 15:37:42 +0600

    Create NewType.rs

---
+++ Rust By Example/Types/NewType.rs
@@ -0,0 +1,9 @@
+use core::num;
+
+fn main() {
+    let numbers = vec![1,2,3,4,5,6];
+    println!("Numbers {:?} ",numbers);
+
+    let strings = vec!["Hello" , "Rust"];
+    println!("Strings {:?}", strings);
+}
