30797f39ec1a4cac3271509e2b874fb5ecc99ac7
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-07-06 18:43:15 +0600

    Create RustNestedIfElse.rs

---
+++ Rust By Example/Flow Of Control/RustNestedIfElse.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let number =15;
+
+    if number > 10 {
+        if number > 20{
+            println!("Number is greater than 10");
+        } else {
+            println!("Number is between 10 and 20");
+        }
+    } else {
+        println!("Number is 10 or less");
+    }
+}
