365d715bf0ff29a80e07f5e30e7b5814e96cf7e5
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-07-06 17:00:24 +0600

    Create TryFrom.rs

---
+++ Rust By Example/Conversion/TryFrom.rs
@@ -0,0 +1,22 @@
+
+use std::convert::TryFrom;
+
+#[derive(Debug)]
+struct EvenNumber(i32);
+
+impl TryFrom<i32> for EvenNumber {
+    type Error = &'static str;
+
+    fn try_from(value: i32) -> Result<Self , Self::Error> {
+        if value % 2 == 0 {
+            Ok(EvenNumber(value))
+        } else {
+            Err("Not an even Number")
+        }
+    }
+}
+
+fn main() {
+    let result = EvenNumber::try_from(8);
+    println!("{:?}", result);
+}
