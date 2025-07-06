c31f77a42ed0435d60db62575937c7db9537aa4f
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-07-06 18:54:20 +0600

    Create RustLogicalAndOperator.rs

---
+++ Rust By Example/Flow Of Control/RustLogicalAndOperator.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let x = 5;
+    let y = 10;
+
+    if x > 0 && y> 0 {
+        println!("Both x and y are positive");
+    } else {
+        println!("Atleast one of the x and y is not positive");
+    }
+}
