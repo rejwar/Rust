56d99b286c22cdae6b9ccd20d000aee2ee78be25
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-07-06 13:50:23 +0600

    Create PointerCastion.rs

---
+++ Rust By Example/Types/PointerCastion.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let x =1;
+    let is_true = x!=0;
+    println!("X : {}, is_true {}", x , is_true);
+
+    let y =0;
+    let is_false = y!=0;
+    println!(" y : {} is false: {}", y, is_false);
+}
