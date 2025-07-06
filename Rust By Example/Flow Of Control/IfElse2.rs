aa7402b3a8bb266ae0b86c79fca7ac27bfc219e3
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-07-06 18:32:08 +0600

    Create IfElse2.rs

---
+++ Rust By Example/Flow Of Control/IfElse2.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let number = 7;
+
+    let result = if number < 5 {
+        "Less than 5"
+    } else if number < 10 {
+        "Between 5 and 10"
+    } else {
+        "10 or greater "
+    };
+
+}
