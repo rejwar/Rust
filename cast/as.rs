357185f97086a3d24be329abed9ead425490562b
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-07-15 04:44:54 +0600

    Both add

---
+++ cast/as.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let FloatNum: f64 = 9.99;
+    let IntNum: i32 = FloatNum as i32;
+
+    println!("Float {}", FloatNum);
+    println!(" Integer is {}", IntNum);
+    
+}
---
+++ cast/as2.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let symbol: char = '@';
+    let code: u8 = Symbol as u8;
+
+    println!("ASCII of {} : {} ", symbol, code);
+}
