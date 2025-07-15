69e7948b62135d77a8725212e0310626dbdb0faa
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-07-15 23:31:17 +0600

    Push:

---
+++ vector/vec9.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let mut Data = vec![1,2,3,4,5,6,7];
+
+    let Drained : Vec<_> = Data.drain(2..4).collect();
+    println!("Drained {:?}", Drained);
+    println!("Reamining {:?}", Data);
+
+    let Split = Data.split_off(2);
+    println!("Split part : {:?}", Split);
+    println!("Original after split {:?}", Data);
+}
