3cf67dcb658f6f20326d94325b610c8a65b4dae3
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-07-17 03:50:40 +0600

    nasdjhf

--- Array/basic2.rs
+++ Array/basic2.rs
@@ -13,6 +13,12 @@ fn main() {
     vec.push("Test");
     vec.push("String");
     println!("{:?}", vec);
+
+    vec.reverse();
+    println!("{:?}", vec);
+
+    let mut vect = Vec::<i32>::with_capacity(2);
+    println!("{}", vect.capacity())
 }
 
 
