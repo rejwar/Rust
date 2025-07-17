40229662484801306cdf60b979c9393e83b365de
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-07-17 07:46:04 +0600

    Push

---
+++ Function/function3.rs
@@ -0,0 +1,15 @@
+fn main() {
+    println!("{}", gcd(20, b=4));
+    
+}
+
+fn gcd(mut a: u64 , mut b: u64 ) -> u64 {
+    while a != 0 {
+        if a > b 
+    } let c = a; 
+    a =b ;
+    {
+    a = a%b;    
+    }
+    b
+}
