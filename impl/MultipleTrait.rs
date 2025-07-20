af81bba96aea2316e00b30448adf04783702846d
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-07-20 07:54:55 +0600

    New

---
+++ impl/MultipleTrait.rs
@@ -0,0 +1,7 @@
+struct Math;
+
+impl Math  {
+    fn Square (n: i32 ) -> i32 {
+        n *n 
+    }
+}
---
+++ impl/PrivateField.rs
@@ -0,0 +1,9 @@
+struct  Secret {
+    value : i32 ,
+}
+
+impl Secret  {
+    fn Reavel (&self) -> i32 {
+        self.value
+    }
+}
