75fd387f1c0dff1c7b46ce6dfadfb91c07c6a4a2
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-07-21 11:51:11 +0600

    New code

---
+++ Closure/PassingClosure.rs
@@ -0,0 +1,15 @@
+fn apply_operations<F>(operation: F, x: i32 , y: i32) -> i32 
+
+where 
+    F: Fn(i32 , i32 ) -> i32 ,
+
+{
+    operation(x, y)
+}
+
+fn main() {
+    let add = |x: i32 , y: i32 | -> i32 {x + y};
+
+    let result = apply_operations(add, 5, 10);
+    println!("Result {}", result);
+}
