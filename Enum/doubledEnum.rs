2bc9c79388c86cfc84f162717f1bb1daf50b30cc
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-08-11 11:21:21 +0600

    new code

---
+++ Enum/doubledEnum.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let value: Option<i32> = None;
+    let doubled = value.map(|n|n*2);
+    println!("{:?}", doubled);
+}fn main() {
+    let value: Option<i32> = None;
+    let doubled = value.map(|n|n*2);
+    println!("{:?}", doubled);
+}
