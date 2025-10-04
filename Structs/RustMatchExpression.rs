137e08149f82dea4f5c87cb8ae47f0194eb1274a
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-10-04 09:11:06 +0600

    new code

--- Structs/RustMatchExpression.rs
+++ Structs/RustMatchExpression.rs
@@ -1,11 +1,3 @@
 fn main() {
-    let number = 7;
-
-    match number {
-        1 => println!(" How are u folks "),
-        2 => println!(" Kaisa ho vai "),
-        3 => println!("Kitni hua jee"),
-        7 => println!(" I am roll number 7"),
-        _=> println!("there are something else"),
-    }
+    
 }
