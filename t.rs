b6ef0c5eb7d0100fdf0cbe81a53018837ed533de
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-10-08 06:44:20 +0600

    new code

--- t.rs
+++ t.rs
@@ -1,1 +1,17 @@
-k.j
+use std::collections::HashMap;
+
+fn main() {
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Red"), 50);
+
+    let red_scores = scores.get("Red");
+    if let Some(&score) = red_scores {
+        println!("Red has {}", score);
+
+    }
+
+    for (team , score) in &scores {
+            println!(" team score : {} {}",team, scores);
+    }
+}
