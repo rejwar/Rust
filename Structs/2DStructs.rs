e48d7849619355711e32a4b09cf2b856f3c15924
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-11-27 17:15:40 +0600

    new ocde

---
+++ Structs/2DStructs.rs
@@ -0,0 +1,38 @@
+struct Vec2 {
+    x: f64,
+    y: f64,
+}
+
+impl Vec2 {
+    fn new(x: f64, y: f64) -> Self {
+        Self { x, y }
+    }
+
+    fn length(&self) -> f64 {
+        (self.x * self.x + self.y * self.y).sqrt()
+    }
+
+    fn add(&self, other: &Self) -> Self {
+        Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+
+    fn scale(&mut self, factor: f64) {
+        self.x *= factor;
+        self.y *= factor;
+    }
+}
+
+fn main() {
+    let mut a = Vec2::new(3.0, 4.0);
+    let b = Vec2::new(1.0, 2.0);
+
+    let c = a.add(&b);
+    println!("c = ({}, {})", c.x, c.y);
+
+    println!("a length = {}", a.length());
+    a.scale(2.0);
+    println!("a after scale = ({}, {})", a.x, a.y);
+}
