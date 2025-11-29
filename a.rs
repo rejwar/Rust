726dec1ee8354cf5f5be6d3a6c480fec22be7f0e
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-11-29 11:00:45 +0600

    new code

--- a.rs
+++ a.rs
@@ -1,1 +1,14 @@
-jb
+struct Circle {
+    radius: f64,
+}
+
+impl Circle {
+    fn new(r: f64) -> Circle {
+        // associated function
+        Circle { radius: r }
+    }
+}
+fn main() {
+    let c = Circle::new(5.0);
+    println!("Circle radius: {}", c.radius);
+}
