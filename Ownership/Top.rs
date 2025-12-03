18c076bcdddee994084bf9693cbdbb4434c86dde
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-12-03 20:59:52 +0600

    New code

---
+++ Ownership/Top.rs
@@ -0,0 +1,30 @@
+// // // // fn main() {
+// // // //     let s1 = String::from("Hello");
+// // // //     let s2 = s1;
+// // // //     println!("{}", s2);
+// // // // }
+
+// // // fn main() {
+// // //     let s1 = String::from("Hello");
+// // //     let s2 = s1;
+// // //     println!("{}", s2);
+// // // }
+
+// // fn main() {
+// //     let s1 = String::from("Heool");
+// //     let s2 = s1.clone();
+// //     println!("S1 is {}", s1);
+// //     println!("S2 is {}", s2);
+// //
+
+// fn main() {
+//     let s = String::from("Hello");
+//     println!("{}", s);
+//
+
+fn main() {
+    let x = 5;
+    let y = x;
+
+    println!("{} {}", x, y);
+}
