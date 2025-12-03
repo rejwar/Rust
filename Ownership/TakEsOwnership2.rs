1ebd7c528d1939a9bc6207ab74f311082116f9cd
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-12-03 21:41:07 +0600

    new code

---
+++ Ownership/TakEsOwnership2.rs
@@ -0,0 +1,17 @@
+// fn takes_ownership(s: String) {
+//     println!("{}", s)
+// }
+
+// fn main() {
+//     let s1 = String::from("Hello");
+//     takes_ownership(s1);
+// }
+
+fn TakesOwnership(s: String) {
+    println!("{}", s);
+}
+
+fn main() {
+    let s1 = String::from("Hello");
+    TakesOwnership(s1);
+}
