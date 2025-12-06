65b2534833bcd72a990b6c93f499fa2bf3960895
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-12-06 11:21:15 +0600

    New code

---
+++ Ownership/Scope.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let mut s = String::from("hello");
+    {
+        let r1 = &mut s;
+        r1.push_str("World");
+    }
+
+    let r2 = &mut s;
+    r2.push_str("!!!");
+
+    println!("{}", r2);
+}
