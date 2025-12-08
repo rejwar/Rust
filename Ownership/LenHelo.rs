be64fb742c93d35312a88ebf18e97691bca4218e
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-12-08 07:11:42 +0600

    new ocde

--- Ownership/LenHelo.rs
+++ Ownership/LenHelo.rs
@@ -1,9 +1,19 @@
-fn main() {
-    let s1 = String::from("Hello");
+// fn main() {
+//     let s1 = String::from("Hello");
 
+//     let len = calculate_length(&s1);
+
+//     println!(" The length of '{}' {}", s1, len);
+// }
+
+// fn calculate_length(s: &String) -> usize {
+//     s.len()
+// }
+fn main() {
+    let s1 = String::from("hello");
     let len = calculate_length(&s1);
 
-    println!(" The length of '{}' {}", s1, len);
+    println!(" The length of {} is {}", s1, len);
 }
 
 fn calculate_length(s: &String) -> usize {
