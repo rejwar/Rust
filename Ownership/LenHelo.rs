e1f0e579271c0d02bcc994f1f5ffc80b35e2499d
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-12-08 07:00:45 +0600

    new code

---
+++ Ownership/LenHelo.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let s1 = String::from("Hello");
+
+    let len = calculate_length(&s1);
+
+    println!(" The length of '{}' {}", s1, len);
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
