d4fbe1e5f5f81797383e430d4202d28537597240
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-12-09 18:41:06 +0600

    Ownership change

---
+++ Ownership/OwnershipChange.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let x = 10;
+    let y = x;
+    println!("x = {} , y = {}", x, y);
+
+    let s1 = String::from("Hello");
+    let s2 = s1;
+
+    println!(" s1 {} s2 {} ", s1, s2);
+}
