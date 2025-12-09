6bac163c4ee1a8a47df7f33bbe811772970c4e8f
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-12-09 18:07:31 +0600

    Double Scope Rs

---
+++ Ownership/DoubleScope.rs
@@ -0,0 +1,20 @@
+struct MyData {
+    name: String,
+}
+
+impl Drop for MyData {
+    fn drop(&mut self) {
+        println!(" Dropping {}", self.name);
+    }
+}
+
+fn main() {
+    let x = MyData {
+        name: String::from("A"),
+    };
+    let y = MyData {
+        name: String::from("B"),
+    };
+
+    println!("End of main");
+}
