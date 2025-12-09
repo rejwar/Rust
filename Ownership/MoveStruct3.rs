a754a1e468f9f696a670f7dbe3fa43f8bacad20e
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-12-09 18:48:49 +0600

    new code

---
+++ Ownership/MoveStruct3.rs
@@ -0,0 +1,13 @@
+struct Account {
+    username: String,
+    active: bool,
+}
+
+fn main() {
+    let acc = Account {
+        username: String::from("Rifat"),
+        active: true,
+    };
+
+    let name = acc.username;
+}
