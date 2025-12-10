0bc4f4075e9fdcfd5926ac8d5f1a08e4efc34ec6
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-12-10 15:38:01 +0600

    hashmap

---
+++ Ownership/HashmapInsert.rs
@@ -0,0 +1,9 @@
+use std::collections::HashMap;
+
+fn main() {
+    let key = String::from("User12");
+    let value = String::from("admin");
+
+    let mut map = HashMap::new();
+    map.insert(key, value);
+}
