d3d735c40a8f4a4e6b6472ef5e0885c88761c92c
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-12-11 11:39:25 +0600

    Hi

--- Ownership/Captured.rs
+++ Ownership/Captured.rs
@@ -1,10 +1,18 @@
+// fn main() {
+//     let s = String::from("Captured");
+
+//     let c = move || {
+//         println!("{}", s);
+//     };
+
+//     c();
+//     c
+// }
 fn main() {
-    let s = String::from("Captured");
+    let s = String::from("Captired ");
 
     let c = move || {
         println!("{}", s);
     };
-
     c();
-    c
 }
