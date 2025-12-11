ac5145859e44db274517b46ba38ffa4200d00246
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-12-11 11:50:17 +0600

    Iter

--- Ownership/IteratorMove.rs
+++ Ownership/IteratorMove.rs
@@ -1,5 +1,12 @@
+// fn main() {
+//     let v = vec![String::from("A")];
+
+//     for item in v.into_iter() {
+//         println!("{}", item);
+//     }
+// }
 fn main() {
-    let v = vec![String::from("A")];
+    let v = vec![String::from("Hello")];
 
     for item in v.into_iter() {
         println!("{}", item);
