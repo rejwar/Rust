e5b13006c65377bc2bb1aa89801b995bacda693e
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-12-11 15:28:12 +0600

    Cargo

---
+++ Ownership/VecElementBorrow.rs
@@ -0,0 +1,11 @@
+// fn main() {
+//     let v = vec![100, 200];
+//     let first = &v[0];
+//     println!("{}", first);
+// }
+
+fn main() {
+    let v = vec![100, 200];
+    let first = &v[0];
+    println!("{}", first);
+}
