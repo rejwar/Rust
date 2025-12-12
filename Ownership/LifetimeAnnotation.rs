9ed6be942c305624d51396762b06c211f7daa6ef
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-12-12 10:28:40 +0600

    Flow state

---
+++ Ownership/LifetimeAnnotation.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let s1 = String::from("Small");
+    let s2 = String::from("Big String");
+
+    println!("Longest {}", longest(&s1, &s2));
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+a
