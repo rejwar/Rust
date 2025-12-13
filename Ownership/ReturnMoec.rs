39e81fc68835b6866a2d15b0381983bbea7358aa
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-12-13 23:49:35 +0600

    Tuple

--- Ownership/ReturnMoec.rs
+++ Ownership/ReturnMoec.rs
@@ -1,5 +1,7 @@
 // FileName: ReturnOwnedMod.rs
-fn create() -> String { String::from("Hello") }
+fn create() -> String {
+    String::from("Hello")
+}
 fn main() {
     let mut s = create();
     s.push_str(" World");
