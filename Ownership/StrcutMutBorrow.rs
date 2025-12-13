d7e04c590d941909477570ca1e368eae8a1901f7
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-12-13 21:42:50 +0600

    Hello

--- Ownership/StrcutMutBorrow.rs
+++ Ownership/StrcutMutBorrow.rs
@@ -1,15 +1,15 @@
-struct Point {
-    x: i32,
-    y: i32,
-}
+// struct Point {
+//     x: i32,
+//     y: i32,
+// }
 
-fn main() {
-    let mut p = Point { x: 0, y: 0 };
-    move_print(&mut p);
-    println!("Moved to {} {}", p.x, p.y);
-}
+// fn main() {
+//     let mut p = Point { x: 0, y: 0 };
+//     move_print(&mut p);
+//     println!("Moved to {} {}", p.x, p.y);
+// }
 
-fn move_print(pt: &mut Point) {
-    pt.x += 10;
-    pt.y += 12;
-}
+// fn move_print(pt: &mut Point) {
+//     pt.x += 10;
+//     pt.y += 12;
+// }
