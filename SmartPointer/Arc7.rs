2f78c07b160c96a70120c17078e3fc7628261f93
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-12-29 03:52:07 +0600

    Hel

---
+++ SmartPointer/Arc7.rs
@@ -0,0 +1,25 @@
+use std::rc::Rc;
+use std::sync::Arc;
+use std::thread;
+
+// fn main() {
+//     let a = Rc::new(10);
+
+//     let b = Rc::clone(&a);
+
+//     thread::spawn(move || {
+//         println!(" {}", b);
+//     })
+//     .join()
+//     .unwrap();
+// }
+fn main() {
+    let a = Arc::new(10);
+    let b = Arc::clone(&a);
+
+    thread::spawn(move || {
+        println!("{}", b);
+    })
+    .join()
+    .unwrap()
+}
