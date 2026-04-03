8e3d32fefc134a17ae3566397f41abf70d95c7ec
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2026-04-04 00:29:16 +0600

    Updated

--- Basic/PatternBinding.rs
+++ Basic/PatternBinding.rs
@@ -1,8 +1,17 @@
+// fn main() {
+//     let x = 5;
+
+//     match x {
+//         n @ 1..=10 => println!(" Number in ranger {}", n),
+//         _ => println!(" Out of range "),
+//     }
+// }
+
 fn main() {
     let x = 5;
 
     match x {
-        n @ 1..=10 => println!(" Number in ranger {}", n),
+        n @ 1..=10 => println!(" Number in ranger t{}", n),
         _ => println!(" Out of range "),
     }
 }
