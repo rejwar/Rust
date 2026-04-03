2d08ee7ce6653a120990ab5ce898ac2734126756
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2026-04-04 00:06:33 +0600

    Updated

--- Basic/MatchArm.rs
+++ Basic/MatchArm.rs
@@ -7,3 +7,13 @@ fn main() {
         _ => println!(" OTher "),
     }
 }
+
+// fn main() {
+//     let x = 2;
+
+//     match x {
+//         1 => println!(" One"),
+//         2 => println!(" Two"),
+//         _ => println!(" Other "),
+//     }
+// }
